aa8614fa4dbb367038857c1a1fc2c51e0262a222
DongEunLEE <dongeun1014@naver.com>
2021-02-14 22:22:09 +0900

    이미지Fadein효과(미완성)

---
+++ Fadein/js/fadein.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    // console.log($(document).scrollTop());
+    // console.log($('.test').offset().top);
+    // console.log($(window).height()/2);
+
+    // $(document).scroll(function(){
+    //     $scroll = $(document).scrollTop();
+    //     $top = $('.test').offset().top
+    //     $windowH = ($(window).height()/1.5)
+        
+    //     if($scroll >= $top - $windowH){
+    //         if(!$(this).hasClass('scrollEvent')){
+    //         }
+    //         $(this).addClass('scrollEvent');
+    //         $(this).fadeIn(500)
+    //     }else{
+    //         $(this).removeClass('scrollEvent');
+    //     }
+    //     });
+    // $('.test').click(function){
+    //     $(this).fadeOut(500);
+    // }
+})
