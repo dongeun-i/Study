be19ff3eb84a535f3ac87549cb8b7d000efdbccc
DongEunLEE <dongeun1014@naver.com>
2022-04-19 18:00:50 +0900

    reverseNumber 문제풀이

---
+++ coding_test/reverseNumber/reverseNumber.js
@@ -0,0 +1,21 @@
+//문제 설명
+// 자연수 n을 뒤집어 각 자리 숫자를 원소로 가지는 배열 형태로 리턴해주세요. 예를들어 n이 12345이면 [5,4,3,2,1]을 리턴합니다.
+
+// 제한 조건
+// n은 10,000,000,000이하인 자연수입니다.
+// 입출력 예
+// n	return
+// 12345	[5,4,3,2,1]
+
+function solution(n) {
+    var answer = [];
+    let nums = String(n).split("");
+
+    let reverse = nums.reverse()
+    reverse.map(n=>{
+        answer.push(Number(n));
+    })
+    
+
+    return answer;
+}
